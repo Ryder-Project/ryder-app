d09c6eeab4b1440e95c3846bde7a9ead8009a4bd
Samuel <36992355+Samuel-Adeyeye@users.noreply.github.com>
2024-01-29 10:24:00 +0100

    feat: :sparkles: add Rider registration endpoint
    refactored rider registration endpoint code

--- apps/server/src/controllers/riderControllers/authRiders.ts
+++ apps/server/src/controllers/riderControllers/authRiders.ts
@@ -1,24 +1,69 @@
 import { Request, Response } from "express";
+import { Op } from "sequelize";
+import { v4 as uuidV4 } from "uuid";
+import { HTTP_STATUS_CODE } from "../../constants/httpStatusCode";
+import { passwordUtils, PasswordHarsher } from "../../utilities/helpers";
+import logger from "../../utilities/logger";
+import { registerSchema } from "../../utilities/validators";
+import Ryder from "../../models/ryder";
 
-// Sample data storage (replace with database in a real-world scenario)
-const riders: string[] = [];
-
-// Controller function for registering riders
-export const registerRider = async (
-  req: Request,
-  res: Response
-): Promise<void> => {
+export const registerRyder = async (req: Request, res: Response) => {
+  const passwordRegex = passwordUtils.regex;
   try {
-    const { name, email, phone, vehicleType } = req.body;
+    const userValidate = registerSchema.strict().safeParse(req.body);
 
-    if (!name || !email || !phone || !vehicleType) {
-      res.status(400).send("Name, email, phone, and vehicleType are required");
+    if (userValidate.success) {
+      const { firstName, lastName, email, phone, password } = userValidate.data;
+      const newEmail = email.trim().toLowerCase();
+
+      if (!passwordRegex.test(password)) {
+        return res.status(HTTP_STATUS_CODE.BAD_REQUEST).json({
+          message: passwordUtils.error,
+        });
+      }
+      const userExist = await Ryder.findOne({
+        where: {
+          [Op.or]: [{ email: newEmail }, { phone: phone }],
+        },
+      });
+      if (!userExist) {
+        const hashedPassword = await PasswordHarsher.hash(password);
+        const id = uuidV4();
+
+        const user = await Ryder.create({
+          id,
+          firstName,
+          lastName,
+          email: newEmail,
+          city: "",
+          phone,
+          password: hashedPassword,
+          bikeDoc: "",
+          validIdCard: "",
+          passportPhoto: "",
+          isVerified: false,
+        });
+
+        return res.status(HTTP_STATUS_CODE.SUCCESS).json({
+          message: `Registration Successful`,
+          user: {
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+          },
+        });
+      } else {
+        return res.status(HTTP_STATUS_CODE.CONFLICT).send({
+          message: "This account already exist",
+        });
+      }
     }
-
-    riders.push(name);
-    res.status(201).json({ message: "Rider registered successfully" });
   } catch (error) {
-    console.error("Error registering rider:", error);
-    res.status(500).send("Internal server error");
+    logger.error(error);
+    return res.status(HTTP_STATUS_CODE.INTERNAL_SERVER).json({
+      message: [
+        { message: `This is our fault, our team are working to resolve this.` },
+      ],
+    });
   }
 };
