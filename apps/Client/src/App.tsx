ec507793a6996cfe11b2ee963c8f7d19960acc85
Afinjuomo <afinjuomooluwatunde17@gmail.com>
2024-01-30 10:12:59 +0100

    request rider page

--- apps/Client/src/App.tsx
+++ apps/Client/src/App.tsx
@@ -1,14 +1,17 @@
-
-import './App.css'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import './index.css'
+import HomePage from './pages/HomePage'
+import RequestRiderPage from './pages/RequestRiderPage'
 
 function App() {
 
   return (
-    <>
-   
-        
-      
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/request-rider' element={<RequestRiderPage/>} />
+      </Routes>
+    </Router>
   )
 }
 
