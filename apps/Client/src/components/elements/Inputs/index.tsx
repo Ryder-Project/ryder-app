33324a14734c6461d0b2e428bdff3813d74a0c6c
Ikechukwu Odokoro <odokoroikechukwujnr@gmail.com>
2024-01-30 18:04:58 +0100

    code fixes and improvements

--- apps/Client/src/components/elements/Inputs/index.tsx
+++ apps/Client/src/components/elements/Inputs/index.tsx
@@ -1,33 +1,22 @@
 import { ChangeEvent } from "react";
-import "./index.css"
+import "./index.css";
 
 interface InputProps {
-  label: string,
-  placeholder: string,
+  label: string;
+  placeholder: string;
 
-   onChange: (event: ChangeEvent<HTMLInputElement>) => void
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const InputField: React.FC<InputProps> = ({  label, placeholder  }) => {
+export const InputField: React.FC<InputProps> = ({ label, placeholder }) => {
   return (
-    <div className='input-container'>
+    <div className="input-container">
       <label>{label}</label>
-      <div className='input-wrapper'>
-        <input className='input' placeholder={placeholder}  />
+      <div className="input-wrapper">
+        <input className="input" placeholder={placeholder} />
       </div>
     </div>
   );
-}
+};
 
 export default InputField;
-
-
-
-
-
-
-
-
-
-
-
