e474b96b21759e213eee955f3fda161a873a435a
Afinjuomo <afinjuomooluwatunde17@gmail.com>
2024-02-02 01:41:07 +0100

    login page ui

--- apps/Client/src/App.tsx
+++ apps/Client/src/App.tsx
@@ -3,6 +3,7 @@ import "./index.css";
 import HomePage from "./pages/HomePage";
 import RequestRiderPage from "./pages/riderForm/RequestRiderPage";
 import RiderSignUpPage from "./pages/Auth/Rider/SignUp";
+import LoginPage from "./pages/Login/LoginPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/request-rider" element={<RequestRiderPage />} />
         <Route path="/signup/rider" element={<RiderSignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
