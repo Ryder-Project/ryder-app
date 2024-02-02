8d829e5b955e4de3f3fc8d4a42e4822684115d50
Kingsley Ibe <29705378+Harinze@users.noreply.github.com>
2024-02-02 12:55:22 +0100

    Settings (#14)
    * pulled from repo
    
    * pulled from repo
    
    * completed settings page
    
    * updated profile settings
    
    * added profile settings endpoint
    
    * updated edit profile
    
    * pulled from repo
    
    * integrated profile settings
    
    * integrated profile settings
    
    * added clearTimeout
    
    * updated profile settings
    
    * updated login
    
    * updated login
    
    * updated settigs
    
    * code fixes and improvement
    
    ---------
    
    Co-authored-by: Ikechukwu Odokoro <odokoroikechukwujnr@gmail.com>

--- apps/Client/src/App.tsx
+++ apps/Client/src/App.tsx
@@ -3,6 +3,7 @@ import "./index.css";
 import HomePage from "./pages/HomePage";
 import RequestRiderPage from "./pages/riderForm/RequestRiderPage";
 import RiderSignUpPage from "./pages/Auth/Rider/SignUp";
+import RiderProfileSettings from "./pages/RiderProfileSettings";
 import LoginPage from "./pages/Login/LoginPage";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/request-rider" element={<RequestRiderPage />} />
         <Route path="/signup/rider" element={<RiderSignUpPage />} />
+        <Route path="/riderprofilesettings" element={<RiderProfileSettings />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
--- apps/server/src/utilities/validators/index.ts
+++ apps/server/src/utilities/validators/index.ts
@@ -6,5 +6,12 @@ export const registerSchema = z.object({
   lastName: z.string().min(2, "lastname is required"),
   email: z.string().email({ message: "email is invalid" }),
   phone: z.string().min(11, "phone number is required"),
-  password: z.string().min(5, passwordUtils.error),
+   password: z.string().min(5, passwordUtils.error),
+});
+
+export const editRiderProfileSchema = z.object({
+  firstName: z.string().min(2, "firstname is required"),
+  lastName: z.string().min(2, "lastname is required"),
+  email: z.string().email({ message: "email is invalid" }),
+  phone: z.string().min(11, "phone number is required")
 });
