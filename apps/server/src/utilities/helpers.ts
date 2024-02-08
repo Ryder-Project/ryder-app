f92ca003b838467d30d5ba647c52b7c4b8bde432
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2024-02-08 02:56:26 +0100

    feat: customer forgot and reset password implemented

--- apps/server/src/utilities/helpers.ts
+++ apps/server/src/utilities/helpers.ts
@@ -1,6 +1,7 @@
 import { ENV } from "../config";
 import bcrypt from "bcrypt";
 import dotenv from "dotenv";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 dotenv.config();
 
@@ -23,4 +24,67 @@ export class PasswordHarsher {
     const salt = await bcrypt.genSalt(10);
     return await bcrypt.hash(password, salt);
   }
+}
+
+export class Jwt {
+  static async sign<
+    TokenPayload extends Record<
+      string,
+      string | Record<string, unknown> | Array<unknown>
+    >,
+  >(payload: TokenPayload, options?: jwt.SignOptions & { _secret?: string }) {
+    const { _secret, ...restOptions } = options ?? {}
+    return jwt.sign(payload, _secret ?? ENV.JWT_SECRET!, restOptions)
+  }
+
+  static async verify<T extends JwtPayload>(
+    token: string,
+    options?: jwt.VerifyOptions & { complete?: true; _secret?: string }
+  ): Promise<T> {
+    const { _secret, ...restOptions } = options ?? {}
+    return jwt.verify(token, _secret ?? ENV.JWT_SECRET!, restOptions) as T
+  }
+
+  static async isTokenExpired<T extends JwtPayload>(
+    token: string,
+    secret?: string
+  ) {
+    try {
+      const payload = await this.verify(token, {
+        _secret: secret || ENV.JWT_SECRET,
+      })
+      return { data: payload as T, expired: false, valid: true }
+    } catch (e) {
+      if (e instanceof jwt.TokenExpiredError) {
+        return {
+          data: (await jwt.decode(token)) as T,
+          expired: true,
+          valid: true,
+        }
+      } else {
+        const decoded = (await jwt.decode(token)) as JwtPayload
+        return {
+          data: (decoded || {}) as T,
+          expired:
+            (decoded?.exp && decoded?.exp < Math.floor(Date.now() / 1000)) ||
+            true,
+          valid: false,
+          error: e,
+        }
+      }
+    }
+  }
+}
+
+export const generateLongString = (length: number) => {
+  let result = ''
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  const charactersLength = characters.length
+  let counter = 0
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength))
+    counter += 1
+  }
+  return result
 }
